dc26faf33cbbef07efbeb539f4b238f7e4c2624b
sergioguri00 <sergio.guerrero.rivero@estudiantat.upc.edu>
2023-04-08 10:38:11 +0000

    login with google

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  has_one_attached :avatar
+
+ def self.from_omniauth(auth)
+   where(uid: auth.uid).first_or_create do |user|
+     user.email = auth.info.email
+     user.password = Devise.friendly_token[0,20]
+     user.full_name = auth.info.name
+   end
+ end
+
+ def avatar_thumbnail
+     avatar.variant(resize: "150x150!").processed
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :issues
   get 'issues/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
