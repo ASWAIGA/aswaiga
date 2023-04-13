2bafe8314c821cffea83edd9a3dfc7ea7f78f358
joel.rivera <joel.rivera@estudiantat.upc.edu>
2023-04-13 12:48:35 +0000

    Bulk v0

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -60,7 +60,7 @@ class IssuesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_issue
-      @issue = Issue.find(params[:id])
+
     end
 
     # Only allow a list of trusted parameters through.
