ac50422baa814aae9e5e6a3540660bd2408cbd9c
sergioguri00 <sergio.guerrero.rivero@estudiantat.upc.edu>
2023-05-17 16:28:15 +0000

    para clonar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,36 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show ]
+  skip_before_action :verify_authenticity_token, if: -> { request.format.json? }
 
   def index
     @users = User.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
   end
 
   def show
     @user = User.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update(usuario_params)
-    redirect_to @user
+  @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(usuario_params)
+        format.html { redirect_to @user }
+        format.json { render json: @user, status: :ok }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
